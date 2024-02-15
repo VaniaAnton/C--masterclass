f0ebf72a632effdaccfdc8fed8acf47c33feb35e
5_vania_5 <antonivan1234567@gmail.com>
2024-02-15 11:23:52 +0200

    first commit

---
+++ oldStyleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OldStyleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("again");
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
