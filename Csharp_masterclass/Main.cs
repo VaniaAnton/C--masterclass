b43d93d2607cddd50a13d42ac59c74c0ee02b6d3
5_vania_5 <antonivan1234567@gmail.com>
2024-02-15 12:49:35 +0200

    Section 3, methods

--- Csharp_masterclass/Main.cs
+++ Csharp_masterclass/Main.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using Section.First;
+using Section.Third;
+
 namespace Section
 {
 	public class MainClass
 	{
 		static public void Main(string[] args)
 		{
-			OldStyle.Test();
+			Section3.Test();
 		}
 	}
 }
---
+++ Csharp_masterclass/Section3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Section.Third
+{
+	public class Section3
+	{
+            public static void Test()
+            {
+                string n1 = "A";
+                string n2 = "B";
+                string n3 = "C";
+                GreetFriend(n1, n2, n3);
+            }
+
+            static void GreetFriend(string a, string b, string c)
+            {
+                Console.WriteLine("Hi {0}, my Friend", a);
+                Console.WriteLine("Hi {0}, my Friend", b);
+                Console.WriteLine("Hi {0}, my Friend", c);
+            }
+        
+	}
+}
+
