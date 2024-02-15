65ca8b9fd6fd27e284e62381ce6329f499d14385
5_vania_5 <antonivan1234567@gmail.com>
2024-02-15 12:44:22 +0200

    first commit

---
+++ Csharp_masterclass/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Section.First;
+namespace Section
+{
+	public class MainClass
+	{
+		static public void Main(string[] args)
+		{
+			OldStyle.Test();
+		}
+	}
+}
+
---
+++ Csharp_masterclass/Section1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Section.First
+{
+    public class Section1
+    {
+         static public void Test()
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("again");
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
