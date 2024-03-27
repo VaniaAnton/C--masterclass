7e0d29b57163573bcf6a4f8e3dab36963d5028db
5_vania_5 <antonivan1234567@gmail.com>
2024-03-28 01:09:51 +0200

    Parsing Game part 2

--- Csharp_masterclass/Main.cs
+++ Csharp_masterclass/Main.cs
@@ -1,4 +1,4 @@
-﻿using Section.Eleventh;
+﻿using Section.Tenth.Game;
 
 namespace Section
 {
@@ -6,7 +6,7 @@ namespace Section
 	{
 		static public void Main(string[] args)
 		{
-			RegularExpression.Test();
+			ParsingGame.Test();
 
 
 			Console.ReadKey();
--- Csharp_masterclass/ParsingGame.cs
+++ Csharp_masterclass/ParsingGame.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 namespace Section.Tenth.Game
 {
 	public class ParsingGame
@@ -18,8 +20,26 @@ namespace Section.Tenth.Game
 					}
 				}
 			}
-			
+
+            string text = System.IO.File.ReadAllText(@"/Users/5_vania_5/Desktop/Udemy/C# masterclass/Course/Csharp_masterclass/Assets/input2.txt");
+            string pattern = @"\d{3}|\d{2}";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            using (StreamWriter file = new StreamWriter(@"/Users/5_vania_5/Desktop/Udemy/C# masterclass/Course/Csharp_masterclass/Assets/output.txt", true))
+            {
+                foreach (Match match in matchCollection)
+                {
+					int numbers = int.Parse(match.Value);
+					file.Write((char)numbers);
+                }
+            }
+
+
+
         }
-	}
+      
+    }
 }
 
