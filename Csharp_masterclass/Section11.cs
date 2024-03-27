86801340afed35ab98fd87cb6930910fdbd2ca05
5_vania_5 <antonivan1234567@gmail.com>
2024-03-27 23:03:35 +0200

    Enum

--- Csharp_masterclass/Section11.cs
+++ Csharp_masterclass/Section11.cs
@@ -2,6 +2,9 @@
 
 namespace Section.Eleventh
 {
+	enum Day { Mo, Tu, we, Th, Fr, Sa, Su};
+	enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
 	struct Game
 	{
 		public string name;
@@ -9,6 +12,8 @@ namespace Section.Eleventh
 		public double rating;
 		public string releaseDate;
 
+		// cannot create a default constructor, don't support inheritence
+		
 		public void Display()
 		{
             Console.WriteLine("Game 1's name is: {0}", name);
@@ -17,18 +22,28 @@ namespace Section.Eleventh
             Console.WriteLine("Game 1's released is: {0}", releaseDate);
         }
 	}
+
 	public class Section11
 	{
 		public static void Test()
 		{
-			Game game1;
+			//Game game1;
 
-			game1.name = "Pokemon Go";
-			game1.developer = "Niantic";
-			game1.rating = 3.6;
-			game1.releaseDate = "01.07.2016";
+			//game1.name = "Pokemon Go";
+			//game1.developer = "Niantic";
+			//game1.rating = 3.6;
+			//game1.releaseDate = "01.07.2016";
 
-			game1.Display();
+			//game1.Display();
+
+			Day fr = Day.Fr;
+			Day su = Day.Su;
+
+			Day a = Day.Fr;
+
+			Console.WriteLine(fr == a);
+			Console.WriteLine(Day.Mo);
+			Console.WriteLine((int)Month.Feb);
 		}
 	}
 }
